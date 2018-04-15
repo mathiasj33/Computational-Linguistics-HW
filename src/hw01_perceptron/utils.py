db2efee74e51fea5236583e7a9058d737804b4cd
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2018-04-15 12:41:18 +0200

    Implemented all methods in utils

--- src/hw01_perceptron/utils.py
+++ src/hw01_perceptron/utils.py
@@ -1,5 +1,6 @@
 import random
-from nltk import word_tokenize
+from nltk import word_tokenize, FreqDist
+from collections import defaultdict
 
 
 def dot(dictA, dictB):
@@ -22,8 +23,9 @@ class DataInstance:
     @classmethod
     def from_list_of_feature_occurrences(cls, feature_list, label):
         """ Creates feature counts for all features in the list."""
-        feature_counts = dict()
-        # TODO: Ex. 3: create a dictionary that contains for each feature in the list the count how often it occurs.
+        feature_counts = defaultdict(int)
+        for f in feature_list:
+            feature_counts[f] += 1
         return cls(feature_counts, label)
 
     @classmethod
@@ -41,18 +43,31 @@ class Dataset:
 
     def get_topn_features(self, n):
         """ This returns a set with the n most frequently occurring features (i.e. the features that are contained in most instances)."""
-        return set()  # TODO: Ex. 4: Return set of features that occur in most instances.
+        iid = dict()
+        for f in self.feature_set:
+            count = 0
+            for i in self.instance_list:
+                if f in i.feature_counts.keys():
+                    count += 1
+            iid[f] = count
+        sorted_list = sorted([(f,c) for f,c in iid.items()], key=lambda x:x[1], reverse=True)
+        return set([f for f,c in sorted_list[:n]])
 
     def set_feature_set(self, feature_set):
         """
         This restrics the feature set. Only features in the specified set all retained. All other feature are removed
         from all instances in the dataset AND from the feature set."""
-        # TODO: Ex. 5: Filter features according to feature set.
-        pass
+        self.feature_set = feature_set
+        for inst in self.instance_list:
+            keys = set(inst.feature_counts.keys()).intersection(feature_set)
+            inst.feature_counts = {k: inst.feature_counts[k] for k in keys}
 
     def most_frequent_sense_accuracy(self):
         """ Computes the accuracy of always predicting the overall most frequent sense for all instances in the dataset. """
-        return 0.0  # TODO: Ex. 6: Return accuracy of always predicting most frequent label in data set.
+        label_list = [inst.label for inst in self.instance_list]
+        freq_dist = FreqDist(label_list)
+        return freq_dist[freq_dist.max()] / len(label_list)
+
 
     def shuffle(self):
         """ Shuffles the dataset. Beneficial for some learning algorithms."""
